610c7157374177e80537016a22b15db2408a759a
Hulud <alifaisal2005@outlook.com>
2026-06-21 22:47:51 +0500

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/' , (req,res) => {
+    res.send('<h1>hi</h1>')
+})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/twitter' , (req,res) => {
+    res.send('hiteshdotcom')
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+app.get('/' , (req,res) => {
+    res.send('<h1>hi</h1>')
+})
