8184b5b11b1b20c0f88e298b8b391d2adc7afb46
Hulud <alifaisal2005@outlook.com>
2026-06-27 13:50:18 +0500

    Starting Lec 14

---
+++ Lecture 14/utils/asynchandler.js
@@ -0,0 +1,13 @@
+const asynchandler = (func) => async (req, res, next) => {
+	try {
+		await func(req, res, next);
+	} catch (error) {
+		const statusCode = error?.statuscode || error?.statusCode || 500;
+		return res.status(statusCode).json({
+			success: false,
+			message: error?.message || "Internal Server Error",
+			errors: error?.errors || [],
+		});
+	}
+};
+export default asynchandler;
