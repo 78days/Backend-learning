1599ff455ed56e6c48154e9783c85a0d1dad74ba
Hulud <alifaisal2005@outlook.com>
2026-06-27 15:00:17 +0500

    Lec 15 started

---
+++ Lecture 15/controllers/user.controller.js
@@ -0,0 +1,49 @@
+import user from "../models/user.model.js";
+import { apierrorhandler } from "../utils/apierror.js";
+import { apiresponse } from "../utils/apiresponse.js";
+import asynchandler from "../utils/asynchandler.js";
+import { fileupload } from "../utils/cloudinary.js";
+
+export const registeruser = asynchandler(async (req, res) => {
+	const { fullname, email, username, password } = req.body;
+
+	const missingfield = [fullname, email, username, password].some(
+		(field) => !field || field.trim() === "",
+	);
+	if (missingfield)
+		throw new apierrorhandler(400, "all fields are required");
+
+	const existeduser = await user.findOne({
+		$or: [{ username: username.toLowerCase() }, { email: email.toLowerCase() }],
+	});
+	if (existeduser) throw new apierrorhandler(409, "user already exists");
+
+	const avatarlocalpath = req.files?.avatar?.[0]?.path;
+	const coverimagepath = req.files?.coverimage?.[0]?.path;
+	if (!avatarlocalpath)
+		throw new apierrorhandler(400, "avatar file is required");
+
+	const avatarimage = await fileupload(avatarlocalpath);
+	const coverimage = coverimagepath
+		? await fileupload(coverimagepath)
+		: null;
+	if (!avatarimage) throw new apierrorhandler(500, "avatar upload failed");
+
+	const userresponse = await user.create({
+		fullname,
+		avatar: avatarimage.url,
+		coverimage: coverimage?.url || "",
+		email: email.toLowerCase(),
+		password,
+		username: username.toLowerCase(),
+	});
+
+	const usercreated = await user
+		.findById(userresponse._id)
+		.select("-password -refreshtoken");
+	if (!usercreated) throw new apierrorhandler(500, "server error");
+	//console.log(req.files)
+	return res
+		.status(201)
+		.json(new apiresponse(201, usercreated, "user created successfully"));
+});
---
+++ Lecture 15/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoschema = new mongoose.Schema(
+	{
+		videofile: {
+			type: String,
+			required: true,
+		},
+		thumbnail: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		duration: {
+			type: String,
+			required: true,
+		},
+
+		views: {
+			type: Number,
+			default: 0,
+		},
+		ispublished: {
+			type: Boolean,
+			default: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "user",
+		},
+	},
+	{ timestamps: true },
+);
+
+videoschema.plugin(aggregatePaginate);
+
+export const video = mongoose.model("video", videoschema);
